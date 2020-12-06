94747d73f5ed3db09771f960c2f0a35c8a8a3c56
George <unnamedgeorge@gmail.com>
2020-12-06 15:29:18 +0200

    Quick Fix

--- update.sh
+++ update.sh
@@ -4,12 +4,17 @@ echo Moving files..
 
 mkdir /var/www/stat/
 mkdir /var/www/stat/stat/
+mkdir /var/www/stat/stat/css/
+mkdir /var/www/stat/stat/Front/fonts/font-awesome/
+mkdir /var/www/stat/stat/Front/images/
+mkdir /var/www/stat/stat/Front/js/
+mkdir /var/www/stat/stat/json-files/
 mv Nginx/s2.nginx /etc/bigbluebutton/nginx/s2.nginx
 mv Back/s2.py /home/s2.py
 mv Front/css /var/www/stat/stat/css/
-mv Front/fonts/font-awesome/ /var/www/stat/stat/Front/fonts/font-awesome/
-mv Front/images/ /var/www/stat/stat/Front/images/
-mv Front/js/ /var/www/stat/stat/Front/js/
+mv Front/fonts/font-awesome/ /var/www/stat/stat/fonts/font-awesome/
+mv Front/images/ /var/www/stat/stat/images/
+mv Front/js/ /var/www/stat/stat/js/
 mv Front/json-files/ /var/www/stat/stat/json-files/
 
 echo Finished.
@@ -17,6 +22,10 @@ echo Finished.
 echo Install requirements...
 
 pip3 install -r requirements.txt
+
+echo Install Apache modules...
+
+apt install apache2-utils -y
 
 echo Restart Nginx...
 
