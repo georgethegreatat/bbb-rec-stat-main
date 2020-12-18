7d64b892400bbc2f2e55d1cb727de85406c052d9
George <unnamedgeorge@gmail.com>
2020-12-18 16:18:23 +0200

    update

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd /home/bbb-rec-stat-main/
 echo Cloning repository...
 git pull
 echo Updating script
 sudo rm /home/s2.py
-sudo mv /home/bbb-rec-stat-main/s2.py /home/s2.py
+sudo cp /home/bbb-rec-stat-main/Back/s2.py /home/s2.py
 echo Done.
+echo Testing script...
+python3 /home/s2.py
+echo Script working! Have a nice day ;)
 echo Please do not forget about adding cron-job for sript. More info: https://github.com/georgethegreatat/bbb-rec-stat-main
