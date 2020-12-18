51c80b57d1d8b4aec7bb36fab4e03fc982f31f50
George <unnamedgeorge@gmail.com>
2020-12-18 16:12:49 +0200

    Adding update script

--- update.sh
+++ update.sh
@@ -1,34 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
-echo Moving files..
-
-mkdir /var/www/stat/
-mkdir /var/www/stat/stat/
-mkdir /var/www/stat/stat/css/
-mkdir /var/www/stat/stat/Front/fonts/font-awesome/
-mkdir /var/www/stat/stat/Front/images/
-mkdir /var/www/stat/stat/Front/js/
-mkdir /var/www/stat/stat/json-files/
-mv Nginx/s2.nginx /etc/bigbluebutton/nginx/s2.nginx
-mv Back/s2.py /home/s2.py
-mv Front/css /var/www/stat/stat/css/
-mv Front/fonts/font-awesome/ /var/www/stat/stat/fonts/font-awesome/
-mv Front/images/ /var/www/stat/stat/images/
-mv Front/js/ /var/www/stat/stat/js/
-mv Front/json-files/ /var/www/stat/stat/json-files/
-
-echo Finished.
-
-echo Install requirements...
-
-pip3 install -r requirements.txt
-
-echo Install Apache modules...
-
-apt install apache2-utils -y
-
-echo Restart Nginx...
-
-service nginx restart
-
-echo Finished successfully.
+cd /home/bbb-rec-stat-main/
+echo Cloning repository...
+git pull
+echo Updating script
+sudo rm /home/s2.py
+sudo mv /home/bbb-rec-stat-main/s2.py /home/s2.py
+echo Done.
+echo Please do not forget about adding cron-job for sript. More info: https://github.com/georgethegreatat/bbb-rec-stat-main
