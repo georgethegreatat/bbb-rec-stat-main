199de3a7465acd90354da52caa66ba18f70584d7
George <unnamedgeorge@gmail.com>
2020-12-18 16:22:35 +0200

    update

--- update.sh
+++ update.sh
@@ -8,5 +8,5 @@ sudo cp /home/bbb-rec-stat-main/Back/s2.py /home/s2.py
 echo Done.
 echo Testing script...
 python3 /home/s2.py
-echo Script working! Have a nice day ;)
+echo Script working! Have a nice day.
 echo Please do not forget about adding cron-job for sript. More info: https://github.com/georgethegreatat/bbb-rec-stat-main
